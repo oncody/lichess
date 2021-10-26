fd496a6defb9a800e04c4b4866a73b1265d63773
Cody Smith <37386486+oncody@users.noreply.github.com>
2021-10-26 07:41:29 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+localStorage.setItem('analyse.ceval.hash-size', '10000');
+localStorage.setItem('analyse.ceval.threads', '20');
+localStorage.setItem('analyse.ceval.multipv', '10');
+localStorage.setItem('analyse.show-move-annotation', 'true');
+localStorage.setItem('analyse.show-computer', 'true');
+localStorage.setItem('analyse.show-auto-shapes', 'true');
+localStorage.setItem('analyse.show-gauge', 'true');
+localStorage.setItem('analyse.ceval.infinite', 'true');
+localStorage.setItem('analyse.treeView', 'column');
+localStorage.setItem('analyse.explorer.enabled', 'true');
+localStorage.setItem('analyse.ceval.enable-nnue', 'true');
+localStorage.setItem('explorer.speed', '["rapid","classical"]');
+localStorage.setItem('explorer.rating', '[2000,2200,2500]');
+localStorage.setItem('analyse.explorer.db.standard', 'lichess');
+
+
+localStorage.setItem('analyse.explorer.db.standard', 'masters');
+
